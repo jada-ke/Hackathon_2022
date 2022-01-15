d05710cbe1934ef4370d1204731e3d86874df7a5
BÜCK-KAEFFER Aurélien <scezaquer@gmail.com>
2022-01-15 13:34:38 -0500

    finalized webapp initialization

--- main.py
+++ main.py
@@ -3,16 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def homepage():
-    return render_template("page.html", title="HOME PAGE")
-
-@app.route("/docs")
-def docs():
-    return render_template("page.html", title="docs page")
-
-@app.route("/about")
-def about():
-    return render_template("page.html", title="about page")
-
+def home():
+    return render_template("index.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
