040345524f82b6de1fe95daeed241d4769f79a5a
BÜCK-KAEFFER Aurélien <scezaquer@gmail.com>
2022-01-15 15:49:44 -0500

    Made an inflation chart

--- inflation.py
+++ inflation.py
@@ -5,10 +5,12 @@ def import_inflation_data(filename, start_year):
     inflation_rates = pd.read_csv(filename, sep = "\t")
     row_nbr = 2021-start_year
     
-    infl = [100]
+    infl = [inflation_rates.iloc[row_nbr]["inflation"]]
     for x in range(row_nbr-1, -1, -1):
-        infl.append(infl[-1]*(1+inflation_rates.iloc[x]["inflation"]/100))
+        infl.append(((1+infl[-1]/100)*(1+inflation_rates.iloc[x]["inflation"]/100)-1)*100)
+    for x, elem in enumerate(infl):
+        infl[x] = round(elem, 2)
     years = [2021-x for x in range(row_nbr, -1, -1)]
-    total_inflation = infl[-1]/100
+    total_inflation = infl[-1]
 
     return infl, years, total_inflation
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from inflation import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    infl, years, total_inflation = import_inflation_data("Data/inflation.csv", 2000)
+    return render_template("index.html", years=years, inflation_values=infl, inflation_total=total_inflation)
     
 if __name__ == "__main__":
     app.run(debug=True)
