706bc0fc523fded5a621ff9910fc750a4322f76e
BÜCK-KAEFFER Aurélien <scezaquer@gmail.com>
2022-01-15 16:46:16 -0500

    updated inflation graph

--- inflation.py
+++ inflation.py
@@ -4,12 +4,14 @@ def import_inflation_data(filename, start_year):
     inflation_rates = pd.read_csv(filename, sep = "\t")
     row_nbr = 2021-start_year
     
-    infl = [inflation_rates.iloc[row_nbr]["inflation"]]
+    cumulative_infl = [inflation_rates.iloc[row_nbr]["inflation"]]
+    yearly_infl = [inflation_rates.iloc[row_nbr]["inflation"]]
     for x in range(row_nbr-1, -1, -1):
-        infl.append(((1+infl[-1]/100)*(1+inflation_rates.iloc[x]["inflation"]/100)-1)*100)
-    for x, elem in enumerate(infl):
-        infl[x] = round(elem, 2)
+        cumulative_infl.append(((1+cumulative_infl[-1]/100)*(1+inflation_rates.iloc[x]["inflation"]/100)-1)*100)
+        yearly_infl.append(inflation_rates.iloc[x]["inflation"])
+    for x, elem in enumerate(cumulative_infl):
+        cumulative_infl[x] = round(elem, 2)
     years = [2021-x for x in range(row_nbr, -1, -1)]
-    total_inflation = infl[-1]
+    total_inflation = cumulative_infl[-1]
 
-    return infl, years, total_inflation
+    return cumulative_infl, years, total_inflation, yearly_infl
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    infl, years, total_inflation = import_inflation_data("Data/inflation.csv", 2000)
-    return render_template("index.html", years=years, inflation_values=infl, inflation_total=total_inflation)
+    cumulative_infl, years, total_inflation, yearly_infl = import_inflation_data("Data/inflation.csv", 2000)
+    return render_template("index.html", years=years, cumulative_infl=cumulative_infl, yearly_infl=yearly_infl, inflation_total=total_inflation)
     
 if __name__ == "__main__":
     app.run(debug=True)
