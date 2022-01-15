bc8b7480e111c1e451a384ac50096f4269a0d4dc
jada-ke <58377404+jada-ke@users.noreply.github.com>
2022-01-15 17:04:34 -0500

    Update scrape_data.py

--- scrape_data.py
+++ scrape_data.py
@@ -1,6 +1,12 @@
 from numpy import string_
 import pandas as pd
-workbook= pd.read_excel("2a71-das-wage2021opendata-esdc-all-19nov2021-vf")
-job= input(str("Please entre your job"))
-index= workbook["NOC_Title"]== job
-workbook[index]
+workbook= pd.read_csv(r"Data/2a71-das-wage2021opendata-esdc-all-19nov2021-vf.csv")
+job= input(str("Please enter your job:"))
+location= input(str("Please enter your location:"))
+index= workbook.loc[(workbook["NOC_Title"]== job) & (workbook["PROV"]==location)]
+
+print(index)
+low_wage= index["Low_Wage_Salaire_Minium"].mean()
+high_wage=index["High_Wage_Salaire_Maximal"].mean()
+median_wage= index["Median_Wage_Salaire_Median"].mean()
+print(low_wage, high_wage, median_wage)
