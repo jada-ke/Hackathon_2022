ab05223ffaa2a8622f03b6798e3c640dde48377a
BÜCK-KAEFFER Aurélien <scezaquer@gmail.com>
2022-01-15 22:27:54 -0500

    fixed indexes

--- salary_distribution.py
+++ salary_distribution.py
@@ -7,8 +7,8 @@ def salary_distribution(locations, job, wage_file):
     salary_dist={}
     for location in locations:
         min_salary=read_data(wage_file, job, location)[0]
-        median_salary=read_data(wage_file, job, location)[1]
-        max_salary=read_data(wage_file, job, location)[2]
+        median_salary=read_data(wage_file, job, location)[2]
+        max_salary=read_data(wage_file, job, location)[1]
         salary_range= [min_salary, max_salary, median_salary]
         salary_dist[location]=salary_range
 
