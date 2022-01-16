79d8da579f81ebe69ed0fed6c7a5beba21253ef3
jada-ke <58377404+jada-ke@users.noreply.github.com>
2022-01-15 19:47:54 -0500

    Update scrape_data.py

--- scrape_data.py
+++ scrape_data.py
@@ -1,10 +1,11 @@
 from numpy import string_
 import pandas as pd
 wage_file=r"Data/2a71-das-wage2021opendata-esdc-all-19nov2021-vf.csv"
-def read_data(wage_file):
+job= input(str("Please enter your job:"))
+location= input(str("Please enter your location:"))
+
+def read_data(wage_file, job, location):
     workbook= pd.read_csv(wage_file)
-    job= input(str("Please enter your job:"))
-    location= input(str("Please enter your location:"))
     index= workbook.loc[(workbook["NOC_Title"]== job) & (workbook["PROV"]==location)]
 
     low_wage= index["Low_Wage_Salaire_Minium"].mean()
